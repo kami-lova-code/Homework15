fec7b4c9ad0a5a9dbf272043452279148b72c6df
Джамиля <rabiyatochka2017@yandex.ru>
2026-08-27 17:19:36 +0300

    Создала приложение

---
+++ org/skypro/skyshop/product/Product.java
@@ -0,0 +1,47 @@
+package org.skypro.skyshop.product;
+
+public class Product {
+
+    private final String name;
+    private final int price;
+
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
